b3d54ff1caa56a742dab9abc4a61daf6d0575eb4
code-don <arthurkoton@proton.me>
2023-12-21 20:15:45 -0500

    route-reverted

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 
 const authController = require("../controllers/authController");
 
-router.post("https://chirpskykite-server.onrender.com/login", authController.login);
+router.post("/login", authController.login);
 
-router.post("https://chirpskykite-server.onrender.com/register", authController.register, authController.sendOTP);
-router.post("https://chirpskykite-server.onrender.com/verify", authController.verifyOTP);
-router.post("https://chirpskykite-server.onrender.com/send-otp", authController.sendOTP);
+router.post("/register", authController.register, authController.sendOTP);
+router.post("/verify", authController.verifyOTP);
+router.post("/send-otp", authController.sendOTP);
 
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password", authController.resetPassword);
--- routes/user.js
+++ routes/user.js
@@ -10,13 +10,13 @@ router.post(
   userController.generateZegoToken
 );
 
-router.get("https://chirpskykite-server.onrender.com/get-call-logs", authController.protect, userController.getCallLogs);
-router.get("https://chirpskykite-server.onrender.com/get-me", authController.protect, userController.getMe);
-router.patch("https://chirpskykite-server.onrender.com/update-me", authController.protect, userController.updateMe);
-router.get("https://chirpskykite-server.onrender.com/get-all-verified-users", authController.protect, userController.getAllVerifiedUsers);
-router.get("https://chirpskykite-server.onrender.com/get-users", authController.protect, userController.getUsers);
-router.get("https://chirpskykite-server.onrender.com/get-requests", authController.protect, userController.getRequests);
-router.get("https://chirpskykite-server.onrender.com/get-friends", authController.protect, userController.getFriends);
+router.get("/get-call-logs", authController.protect, userController.getCallLogs);
+router.get("/get-me", authController.protect, userController.getMe);
+router.patch("/update-me", authController.protect, userController.updateMe);
+router.get("/get-all-verified-users", authController.protect, userController.getAllVerifiedUsers);
+router.get("/get-users", authController.protect, userController.getUsers);
+router.get("/get-requests", authController.protect, userController.getRequests);
+router.get("/get-friends", authController.protect, userController.getFriends);
 
 router.post("/start-audio-call", authController.protect, userController.startAudioCall);
 router.post("/start-video-call", authController.protect, userController.startVideoCall);
